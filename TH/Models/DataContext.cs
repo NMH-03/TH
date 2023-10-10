540568c372e481e5fabe3e49721609d2dcd31fd4
vuongpc <vuongpc@DESKTOP-VEPK6JD>
2023-10-10 18:16:42 +0700

    Add project files.

---
+++ TH/Models/DataContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TH.Models;
+
+namespace TH.Models
+{
+	public class DataContext : DbContext
+	{
+		public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+		public DbSet<Menu> Menus { get; set; }
+		public DbSet<Post> Posts { get; set; }
+	}
+}
